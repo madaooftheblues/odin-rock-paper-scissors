7807a8ca039c072f83fd168cab92a8fa0e6ec5fb
madaooftheblues <azlanrasool@gmail.com>
2022-07-27 16:06:20 +0500

    Add function to get computer's choice
    getComputerChoice() fucntion is added that evaluates the computer's choice randomly by the use of
    Math.random() and Math.floor() library functions.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+//We initialize three choices in an array (Rock, Paper, Scissors)
+let choices = ["Rock", "Paper", "Scissors"];
+
+//We fetch the computer's choice by generating a random number that corresponds to the index of choices array
+function getComputerChoice() {
+  return Math.floor(Math.random() * choices.length);
+}
+
+console.log(getComputerChoice());
